16046627f5a34212ee8c7279f225b821cee85c2d
Christian David G <cdavid.ggarcia@gmail.com>
2022-12-08 14:29:13 +0100

    feat: check ts

--- web/gatsby-config.ts
+++ web/gatsby-config.ts
@@ -1,18 +1,38 @@
-import type { GatsbyConfig } from "gatsby"
-
-require("dotenv").config({
-  path: ".env.development",
-})
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: "Luxwook",
-    siteUrl: "https://luxwook.netlify.app",
-    description: "Luxwood",
+    title: `Example TS`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [
-    `gatsby-plugin-sass`,
-  ],
-}
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "testProjectId",
+      "dataset": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "testTrackingId"
+    }
+  }, "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
 
-export default config
+export default config;
