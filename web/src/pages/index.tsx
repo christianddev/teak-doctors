1d4e01d7c70e02d1fb612a0538b7fc91eda889a9
Christian David G <cdavid.ggarcia@gmail.com>
2022-12-08 14:47:08 +0100

    feat: check ts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -190,4 +190,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home PageXD</title>
