32fcd92119740ac0db65890d2fe89a8c8986f8c1
Christian G.(WSL) <cdavid.ggarcia@gmail.com>
2023-02-21 06:49:23 +0100

    feat: test TS config

---
+++ web/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+import Header from "./Header"
+
+interface  LayoutProps {
+  children: React.ReactNode
+}
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  )
+}
+
+export default Layout
