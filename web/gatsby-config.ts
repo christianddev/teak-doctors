1514cd709a6c1d091938cb001b2a7489890548b7
Christian G.(WSL) <cdavid.ggarcia@gmail.com>
2023-02-24 06:37:26 +0100

    feat: update siteUrl

--- web/gatsby-config.ts
+++ web/gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `teak-doctors-blog`,
+    title: `Teak-doctors-blog`,
     siteUrl: `https://teak-doctors.netlify.app`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
